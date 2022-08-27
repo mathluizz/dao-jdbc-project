cf6597cc7f6ba28e0fe2909d272cf58b3984fc49
Matheus Oliveira <matheus_luiz2011@hotmail.com>
2022-08-27 01:33:00 -0300

    Update created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,5 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.util.Date;
 import java.util.List;
 
@@ -14,9 +12,6 @@ public class Program {
     public static void main(String[] args){
        
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-
-        PreparedStatement st = null;
-        Connection conn = null;
 
         System.out.println("=== TEST 1: seller findById =====");
 		Seller seller = sellerDao.findById(3);
@@ -39,5 +34,10 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
     }
 }
